2135982365a457645b4fc9d7454e12e8844afcce
Max__La__Menace <maxence.depire@gmail.com>
2023-10-12 21:49:37 +0200

    Adding the julia methods.

---
+++ julia-method.jl
@@ -0,0 +1,25 @@
+### A Pluto.jl notebook ###
+# v0.19.29
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ a0e2c270-6936-11ee-216d-09bba29a35f4
+md"# Calculations of Pi, but in Julia !"
+
+# ╔═╡ 3922ad87-ad20-47b0-bb18-a743ff83f362
+begin
+	diameter = 1
+	sides = 6
+end
+
+# ╔═╡ 87f2b0da-e61d-49d9-a768-4b96745b7609
+begin
+	big_triangle_corner = 360 / diameter
+	little_triangle_corner = ma
+end
+
+# ╔═╡ Cell order:
+# ╠═a0e2c270-6936-11ee-216d-09bba29a35f4
+# ╠═3922ad87-ad20-47b0-bb18-a743ff83f362
+# ╠═87f2b0da-e61d-49d9-a768-4b96745b7609
